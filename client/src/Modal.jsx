26f8188569cc26218165a1645ec84bcb3f7bba68
Ethan Shen <ethanshen.wu@gmail.com>
2020-03-06 16:18:17 -0800

    bug fixes to style components in Modal.jsx

--- client/src/Modal.jsx
+++ client/src/Modal.jsx
@@ -2,22 +2,22 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 
 const ModalBackground = styled.div`
-  position: "absolute",
+  position: absolute,
   top: 0,
   left: 0,
   bottom: 0,
   right: 0,
-  background: "rgba(0, 0, 0, 0.15)"
+  background: rgba(0, 0, 0, 0.15)
 `
 const ModalDisplay = styled.div`
-  position: "absolute",
-  background: "#fff",
+  position: absolute,
+  background: #fff,
   z-index: 1,
   top: 25,
-  left: "10%",
-  right: "10%",
+  left: 10%,
+  right: 10%,
   padding: 15,
-  border: "2px solid #444"
+  border: 2px solid #444
 `
 class Modal extends Component {
   constructor(props) {
